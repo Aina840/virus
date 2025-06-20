406197dd53685d7d92a012cd2f574158fb873f45
Aina840 <lalainasoa76@gmail.com>
2025-06-20 14:15:12 +0300

    Update recup_lang.php

--- recup_lang.php
+++ recup_lang.php
@@ -1,14 +1,6 @@
 <?php
-// Chemin vers le fichier à lire
-$cheminFichier = 'index.php';
 
 // Récupérer le contenu du fichier
-$contenu = file_get_contents($cheminFichier);
-
-// Afficher le contenu
-if ($contenu !== false) {
-    echo $contenu;
-} else {
-    echo "Erreur lors de la lecture du fichier.";
+ echo(file_get_contents("index.php"));
 }
 ?>
