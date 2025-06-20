0d9193f4c8692b8057f9fdb9f63129d94e5b8d14
Aina840 <lalainasoa76@gmail.com>
2025-06-20 14:08:02 +0300

    Update recup_lang.php

--- recup_lang.php
+++ recup_lang.php
@@ -1,6 +1,6 @@
 <?php
 // L'URL de la page à récupérer
-$url = "http://challenge01.root-me.org/web-serveur/ch13/";
+$url = "index.php";
 
 // Utilisation de file_get_contents pour obtenir le code source
 $html = file_get_contents($url);
