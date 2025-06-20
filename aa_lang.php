72224fe43e6c5361713399cfcfef32fde28cf675
Aina840 <lalainasoa76@gmail.com>
2025-06-20 13:54:49 +0300

    Update aa_lang.php

--- aa_lang.php
+++ aa_lang.php
@@ -1,9 +1,4 @@
 <?php
-// Exemple de script vulnérable à une LFI (Local File Inclusion)
-if (isset($_GET['page'])) {
-    // Vulnérabilité LFI : la variable n'est pas filtrée
-    include($_GET['page']);
-} else {
-    echo "Aucune page spécifiée.";
-}
+// Script PHP simple pour afficher "Bonjour"
+echo "Bonjour";
 ?>
