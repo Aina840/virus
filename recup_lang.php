c2a6dd1db322c55b9759380d4af63f3d038f7e87
Aina840 <lalainasoa76@gmail.com>
2025-06-20 14:01:39 +0300

    Create recup_lang.php

---
+++ recup_lang.php
@@ -0,0 +1,10 @@
+<?php
+// L'URL de la page à récupérer
+$url = "https://www.example.com";
+
+// Utilisation de file_get_contents pour obtenir le code source
+$html = file_get_contents($url);
+
+// Affichage du code source
+echo $html;
+?>
